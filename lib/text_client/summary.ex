499349c172ca705fe8fc4b230af52d690ae55ae5
rskvirskiy <rskvirskiy@gmail.com>
2018-12-06 17:08:58 -0500

    Added used letters.

--- lib/text_client/summary.ex
+++ lib/text_client/summary.ex
@@ -3,7 +3,8 @@ defmodule TextClient.Summary do
     IO.puts [
       "\n",
       "Word so far: #{Enum.join(tally.letters, " ")}\n",
-      "Guesses left: #{tally.turns_left}\n"
+      "Guesses left: #{tally.turns_left}\n",
+      "Used letters: #{Enum.join(tally.used_letters, " ")}"
     ]
     game
   end
